89f3ca3f0a49aaf604b522f020a75d469c363ae8
MVK-USA <mvk2015@yahoo.com>
2020-11-10 15:50:09 -0500

    some changes to feature-a

--- src/FeatureAClass.java
+++ src/FeatureAClass.java
@@ -3,5 +3,7 @@ public class FeatureAClass {
     public static void main(String[] args) {
         // added the java file
         // more work here
+        // sun shine brightly
+        // everything is great
     }
 }
